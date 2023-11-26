a0db14c234e85b0e26e339da4ddf3ab6182fced1
gpinheir <gpinheir@cern.ch>
2023-11-26 12:14:27 -0500

    Adding function to plot critical temperature vs. length (part 4 in Le Bellac)

--- analysis.py
+++ analysis.py
@@ -1,11 +1,18 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-from glob import glob
 
 data_path = "./data/"
 plots_path = "./plots/"
 sizes_to_plot = [10, 16, 24, 36]
+
+def critical_values(T, X):
+    # We get some outliers near T = 0, so use this to ignore those
+    peak_window = (T > 1.5) & (T < 3.5)
+
+    # Find T of peak, peak value, and return
+    T, X = T[peak_window], X[peak_window]
+    peak = np.argmax(X)
+    return T[peak], X[peak]
 
 class Plotter:
 
@@ -59,10 +66,34 @@ class Plotter:
             plt.clf()
             plt.close() 
 
+    def plot_critical_temperature(self):
+        fig, ax = plt.subplots()
 
+        critical_temperatures = []
+        inv_L = []
+        accepted_Tc = 2.269185
+
+        for _, L in enumerate(self.sizes):
+            Tc, _ = critical_values(self.results[L]["T"], self.results[L]["Chi"])
+            critical_temperatures.append(Tc); inv_L.append(1/L)
+
+        ax.scatter(inv_L, critical_temperatures, color="k", s=self.marker_size)
+        ax.axhline(accepted_Tc, color="r", ls="--", label=f"Accepted $T_C$ = {accepted_Tc}")
+
+        ax.set(xlabel=r"$L^{-1}$", ylabel=r"$T_{c}(L)$", ylim=(2, 2.5))
+        ax.grid()
+        ax.legend()
+        plt.tight_layout()
+        for ext in ["png", "pdf"]: fig.savefig(plots_path + f"ising_2d_critical_temperature.{ext}")
+
+        plt.cla()
+        plt.clf()
+        plt.close() 
+        
 def main():
     plotter = Plotter(sizes_to_plot)
     plotter.plot_results()
+    plotter.plot_critical_temperature()
 
 if __name__ == "__main__":
     main()
