340a18b898e84512b0705f65318988dfdf7be819
gpinheir <gpinheir@cern.ch>
2023-11-28 17:34:59 -0500

    Cleaning up ising model class, removing cluster algorithm option. Adding some print outs to simulate function, parallelizing loop over temperatures ~x10 speedup

--- ising_model.py
+++ ising_model.py
@@ -3,11 +3,11 @@ import helper
 
 class IsingModel:
 
-    def __init__(self, L, T, J=1, option="metropolis"):
+    def __init__(self, L, T, J=1):
         """
-        Class representing the Ising model for a given lattice size, temperatue, and
-        interaction term, with an option to update the spin configuration using either
-        the Metropolis of cluster algorithms.
+        Class representing the Ising model for a given lattice size, temperature, and
+        interaction term, with an option to update the spin configuration using the
+        Metropolis algorithm
 
         Parameters
         ----------
@@ -19,41 +19,22 @@ class IsingModel:
 
         J : float
             Nearest neighbor interaction constant.
-
-        option : string
-            Update system in a Monte Carlo step using either the metropolis or cluster
-            algorithms
         
         """
         self.L = L      # Lattice size
         self.T = T*J    # Temperature (in units of J)
         self.J = J      # Neighbor interaction strength
 
-        # Do MC step either w/ metropolis or cluster algorithms
-        self.option = self._set_option(option)
-
         self.state = self._get_initial_state()
-
-    def _set_option(self, option):
-        """
-        Sets the MC option to either metropolis or cluster.
-
-        NOTE: the clustering option still needs to be implemented.
-
-        """
-        option = option.lower()
-
-        if option not in ["metropolis", "cluster"]:
-            print(f"MC step option {option} not supported, defaulting to metropolis algorithm...")
-            return "metropolis"
-        
-        else: return option
 
     def _get_initial_state(self):
         """
-        Returns lattice of size LxL with up spins (+1) and down spins (-1) selected randomly.
+        Returns lattice of size LxL with up spins (+1) and down spins (-1). If the temperature
+        is low enough, start with all spins up. This makes the thermalization step more likely
+        to converge. Otherwise, start with a random configuration.
         """
-        return 2*np.random.randint(2, size=(self.L, self.L))-1
+        if self.T / self.J < 1 : return np.ones((self.L, self.L))
+        else: return 2*np.random.randint(2, size=(self.L, self.L))-1
 
     @property
     def total_spins(self):
@@ -80,19 +61,6 @@ class IsingModel:
     
     def mc_step(self):
         """
-        Performs a Monte Carlo (MC) step. 
-
-        If option is to 'metropolis', the metropolis algorithm is used. Otherwise,
-        the cluster algorithm is used (this is useful when we are closer to the
-        critical temperatue, and flipping individual spins becomes probabilistically
-        unfavorable). 
-
-        NOTE: the cluster option still needs to be implemented.
-
+        Performs a Monte Carlo (MC) step with the Metropolis algorithm. 
         """
-        if self.option == "metropolis":
-            helper.mc_step_metropolis(self.state, self.L, self.T, self.J, self.total_spins)
-
-        else:
-            print("Come back later!")
-            return
+        helper.mc_step_metropolis(self.state, self.L, self.T, self.J, self.total_spins) #NOTE this is pass by reference
--- simulate.py
+++ simulate.py
@@ -1,10 +1,11 @@
-import argparse
 import numpy as np
+from functools import partial
+from multiprocessing import Pool, cpu_count
 from ising_model import IsingModel
 
 data_path = "./data/"
 
-def simulate(L, T, thermalize_sweeps, msmt_sweeps, msmt_rate, verbose=False):
+def simulate(thermalize_sweeps, msmt_sweeps, msmt_rate, L, T, verbose=False):
     """
     Runs the simulation for the ising model.
 
@@ -43,13 +44,11 @@ def simulate(L, T, thermalize_sweeps, msmt_sweeps, msmt_rate, verbose=False):
     ising = IsingModel(L, T)
 
     for i in range(thermalize_sweeps):
-        if (i % 10000 == 0) and verbose: print(f"-----> Thermalization sweep #{i}")
         ising.mc_step()
-    
-    print("Done thermalizing!")
+
+    if verbose: print("Done thermalizing!")
 
     for i in range(msmt_sweeps):
-        if (i % 10000 == 0) and verbose: print(f"-----> Measurement sweep #{i}")
         ising.mc_step()
 
         if i % msmt_rate == 0:
@@ -60,7 +59,8 @@ def simulate(L, T, thermalize_sweeps, msmt_sweeps, msmt_rate, verbose=False):
             magnetization += msmt_magnetization
             magnetization_sq += msmt_magnetization**2
 
-    print(f"Done measurements for L = {L}, T = {T}\n")
+    if verbose: print(f"Done measurements for L = {L}, T = {T}!")
+
     return energy/n_msmts, energy_sq/n_msmts, magnetization/n_msmts, magnetization_sq/n_msmts
 
 def save(results, L):
@@ -78,8 +78,25 @@ def save(results, L):
     """
     with open(data_path + f"ising_2d_L_{L}.npy", "wb") as f: np.save(f, results) 
 
+
 #-----------------------------------------------------------------------------
 def main():
+
+    print("")
+    print("~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/")
+    print("")
+    print(" _____    _              ___  ___          _      _ ")
+    print("|_   _|  (_)             |  \/  |         | |    | |")
+    print("  | | ___ _ _ __   __ _  | .  . | ___   __| | ___| |")
+    print("  | |/ __| | '_ \ / _` | | |\/| |/ _ \ / _` |/ _ \ |")
+    print(" _| |\__ \ | | | | (_| | | |  | | (_) | (_| |  __/ |")
+    print(" \___/___/_|_| |_|\__, | \_|  |_/\___/ \__,_|\___|_|")
+    print("                   __/ |                            ")
+    print("                  |___/                             ")
+    print("")
+    print("~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/")
+    print("")
+
 
     # Sweep parameters
     thermalize_sweeps = 10**5
@@ -88,30 +105,45 @@ def main():
 
     # System parameters
     sizes = [10, 16, 24, 36]
-    temperatures = np.arange(0.015, 4.5, 0.015) 
+    T = np.arange(0.015, 4.5, 0.5) 
     k = 1 # Setting Boltzmann constant to 1
+
+    print("Parameters")
+    print("----------")
+    print(f"Thermalization sweeps = {thermalize_sweeps}")
+    print(f"Measurement sweeps    = {msmt_sweeps}")
+    print(f"Measurement rate      = {msmt_rate}")
+    print(f"System sizes          = {sizes}")
+    print(f"Temperature range     = ({T.min()}, {T.max()}, {T.size})")
+    print(f"Boltzmann constant    = {k}")
+    print("")
 
     # Data type definitions for structured array of results
     results_dtype = [("E", float), ("M", float), ("C", float), ("Chi", float), ("T", float)]
 
     for L in sizes:
-        N = L**2 
-        E, M, C, Chi = [], [], [], []                                # Defines lists to store results
-        results = np.empty(temperatures.size, dtype=results_dtype)   # Defines structured array to save
+        print(f"Starting simulation for size L = {L}")
+        N = L**2
+        results = np.empty(T.size, dtype=results_dtype)   # Defines structured array to save
 
-        for i, T in enumerate(temperatures):
-            energy, energy_sq, magnetization, magnetization_sq = simulate(L, T, thermalize_sweeps, msmt_sweeps, msmt_rate)
-
-            E.append(energy)
-            M.append(magnetization)
-            C.append((1/(N*k*T**2))*(energy_sq - energy**2))
-            Chi.append((N/(k*T))*(magnetization_sq - magnetization**2))
+        # Parallelizes process depending on how many CPU cores are available
+        with Pool(cpu_count()) as p:
+            f = partial(simulate, thermalize_sweeps, msmt_sweeps, msmt_rate, L)
+            energy, energy_sq, magnetization, magnetization_sq = map(np.asarray, zip(*p.map(f, T)))
 
         # Saves simulation results for analysis
-        for values, variable in zip([E, M, C, Chi, temperatures], ["E", "M", "C", "Chi", "T"]):
-            results[variable] = np.asarray(values, dtype=float) # Convert to np array before saving
-        
+        results["E"]   = energy
+        results["M"]   = magnetization
+        results["C"]   = (1/(N*k*T**2))*(energy_sq - energy**2)
+        results["Chi"] = (N/(k*T))*(magnetization_sq - magnetization**2) 
+        results["T"]   = T
+
+        print(f"Done simulation for L = {L}!")
+        print("Saving...")
         save(results, L)
+        print("Done!")
+        print("")
+
 
 if __name__ == "__main__":
     main()
